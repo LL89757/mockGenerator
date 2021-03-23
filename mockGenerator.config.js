c4156df0a24572b9672cbad518aca58572341f94
li.lv <li.lv@xiaobaoonline.com>
2021-03-23 16:56:41 +0800

    chore: 配置优化，支持多任务构建

--- mockGenerator.config.js
+++ mockGenerator.config.js
@@ -1,8 +1,49 @@
 module.exports = {
-  swaggerUrl: '', // swagger-api的文档地址
+  swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-edu-admin', // swagger-api的文档地址
+  swaggerUrlList: [
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-edu-admin',
+      outputDir: '/mock/edu-admin',
+      generate: false,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-sso',
+      outputDir: '/mock/sso',
+      generate: false,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-ordering',
+      outputDir: '/mock/ordering',
+      generate: true,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-org-management',
+      outputDir: '/mock/org-management',
+      generate: true,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-bff',
+      outputDir: '/mock/bff',
+      generate: true,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-background-task',
+      outputDir: '/mock/background-task',
+      generate: false,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-msg-center',
+      outputDir: '/mock/msg-center',
+      generate: false,
+    },
+    {
+      swaggerUrl: 'https://dev-tbu-next-erp-gateway.schoolpal.cn/swagger/df-dev-next-erp-020-01/next-report',
+      outputDir: '/mock/report',
+      generate: true,
+    },
+  ],
   outputFileType: 'ts', //生成文件类型 ts||js
-  outputPath: './mock/biz', // 输出目录，最终会自动生成文件 outputPath/[模块名]
-  ignoreList: ['Import'], // 忽略api列表
+  ignoreList: [], // 忽略api列表
   dataLength: '1-8', // mock 为数组时数组长度
   fileName: 'mock', //默认以swaggerapi tags的第一个值为模块名，按模块生成mock文件，否则以该配置为文件名生成单个文件
-};
+}
